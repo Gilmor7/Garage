9460cab4053bba04adfce58291957288edb85dcb
Eli Freid <86243882+E-Freid@users.noreply.github.com>
2023-05-22 22:31:18 +0300

    added abstract truck and FueledTruck classes

--- Ex03.GarageLogic/Truck.cs
+++ Ex03.GarageLogic/Truck.cs
@@ -3,7 +3,24 @@ using System.Collections.Generic;
 
 namespace Ex03.GarageLogic
 {
-    public abstract class Truck
+    public abstract class Truck: Vehicle
     {
+        private const int k_NumOfWheelsOnTruck = 14;
+        private const float k_MaxWheelAirPressureInTruck = 26;
+        private bool m_IsCarryingToxicMaterials;
+        private float m_CargoVolume;
+        protected Truck(string i_LicenseNumber) : base(i_LicenseNumber)
+        {
+            m_NumOfWheels = k_NumOfWheelsOnTruck;
+            m_MaxWheelAirPressure = k_MaxWheelAirPressureInTruck;
+            initializeWheels();
+        }
+        
+        public override void SetMyRequirements()
+        {
+            base.SetMyRequirements();
+            m_Requirements.Add("isCarryingToxicMaterials", "Is carrying toxic materials? (true/false)");
+            m_Requirements.Add("cargoVolume", "Cargo volume");
+        }
     }
 }
